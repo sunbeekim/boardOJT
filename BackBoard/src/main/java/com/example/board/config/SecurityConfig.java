d695429003ff54d4f2865e2e8983982ad368289a
unknown <rlatjsql12@gmail.com>
2025-04-08 17:58:46 +0900

    공통응답구조 확정

--- BackBoard/src/main/java/com/example/board/config/SecurityConfig.java
+++ BackBoard/src/main/java/com/example/board/config/SecurityConfig.java
@@ -34,17 +34,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .csrf().disable() // csrf 보안 비활성화
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용 안함
                 .and()
-                .authorizeRequests()
-                .antMatchers("/api/user/**").permitAll()
-                .antMatchers("/docs/**").permitAll()
-                .antMatchers("/error").permitAll()
-                .anyRequest().authenticated()
+                .authorizeRequests() // 인증 설정 ->
+                .antMatchers("/api/user/**").permitAll() // 엔드포인트 경로 토큰 인증 허용
+                .antMatchers("/docs/**").permitAll() // docs 문서 html 접근경로
+                .antMatchers("/error/**").permitAll() //
+                .anyRequest().authenticated() // 나머지 요청은 인증 필요
                 .and()
+                // 시큐리티 필터체인에 jwt 필터 추가
                 .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                .formLogin().disable()
-                .httpBasic().disable();
+                // 기본 로그인/http 인증 비활성화
+                .httpBasic().disable()
+                .formLogin().disable();
     }
 }
