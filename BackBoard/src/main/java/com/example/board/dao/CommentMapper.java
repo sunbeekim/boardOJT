f9d030f0979d47ef36684c03212b9af6bfd2e5c7
sunbeekim <62640672+sunbeekim@users.noreply.github.com>
2025-04-13 10:47:32 +0000

    test

--- BackBoard/src/main/java/com/example/board/dao/CommentMapper.java
+++ BackBoard/src/main/java/com/example/board/dao/CommentMapper.java
@@ -1,5 +1,20 @@
 package com.example.board.dao;
 
-public class CommentMapper {
-    
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.example.board.domain.comment.dto.CommentResponseDto;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface CommentMapper {
+
+    // 댓글 등록
+    void insertComment(@Param("postId") Long postId, @Param("userId") Long userId, @Param("content") String content);
+
+    // 게시글의 모든 댓글 조회
+    List<CommentResponseDto> getCommentsByPostId(@Param("postId") Long postId, @Param("currentUserId") Long currentUserId);
+
 }
